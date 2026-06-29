1e5b29d3e3e99cb06cf7e62609b46e76e581e81d
joao-menna <joao.menna1907@gmail.com>
2026-06-29 17:58:05 -0300

    Removed accidental tts.rocks

--- tts.rocks/thirdparty/onnxruntime-web.js
+++
@@ -1,8 +0,0 @@
-// ONNX Runtime Web module wrapper
-// This file exports the ort global as an ES module
-
-const ort = window.ort;
-export default ort;
-export const InferenceSession = ort.InferenceSession;
-export const Tensor = ort.Tensor; 
-export const env = ort.env;
