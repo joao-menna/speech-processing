7943e8739048e4a03aa1f565bf1145110a617b0d
joao-menna <joao.menna1907@gmail.com>
2026-07-02 16:39:04 -0300

    Fixed serve-all.ps1

--- serve-all.ps1
+++ serve-all.ps1
@@ -5,6 +5,6 @@ Set-Location ..\tts.rocks
 wt -w 0 --title "TTS" -d . PowerShell -c "npx serve"
 
 Set-Location ..\whisper-web
-wt -w 0 --title "Whisper" -d . PowerShell -c "pnpm dev"
+wt -w 0 --title "Whisper" -d . PowerShell -c "pnpm run dev"
 
 Set-Location ..\
